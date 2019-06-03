0687d5402e9cc342cee210335a10b233e7076175
Saad Baradan <saadbaradan@gmail.com>
2019-06-03 17:32:21 -0600

    Add tests for toggleFavorite, isLoading, and hasErrored

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -10,5 +10,32 @@ describe('actions', () => {
         }
         const result = actions.addBikePaths(bikePaths);
         expect(result).toEqual(expectedAction)
-    })
+    });
+    it('should have a type of TOGGLE_FAVORITE', () => {
+        const id = 'Healthy Rides'
+        const expectedAction = {
+            type: 'TOGGLE_FAVORITE',
+            id
+        }
+        const result = actions.toggleFavorite(id);
+        expect(result).toEqual(expectedAction)
+    });
+    it('should have a type of IS_LOADING', () => {
+        const bool = false
+        const expectedAction = {
+            type: 'IS_LOADING',
+            bool
+        }
+        const result = actions.isLoading(bool);
+        expect(result).toEqual(expectedAction)
+    });
+    it('should have a type of HAS_ERRORED', () => {
+        const bool = false
+        const expectedAction = {
+            type: 'HAS_ERRORED',
+            bool
+        }
+        const result = actions.hasErrored(bool);
+        expect(result).toEqual(expectedAction)
+    });
 })
