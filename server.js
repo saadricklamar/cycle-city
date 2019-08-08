6206167e21401b72f044563fb805fe038bbdea71
Saad Baradan <saadbaradan@gmail.com>
2019-08-08 15:34:58 -0600

    cors

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+const cors = require('cors')
 
-
+app.use(cors( {origin: '*' }))
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
