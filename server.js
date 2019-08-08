c089eb946a483071c10d97b56b29b09d3f7cffd8
Saad Baradan <saadbaradan@gmail.com>
2019-08-08 15:47:02 -0600

    Add procfile

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
 const app = express();
-const cors = require('cors')
+const PORT = process.env.PORT || 5000;
 
-app.use(cors( {origin: '*' }))
-app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
+
+app.get('*', function (req, res) {
+ res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
+
+app.listen(PORT);
