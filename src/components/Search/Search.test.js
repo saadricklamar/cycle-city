584154bbd9a9e05d33b82598c0c5b8d37b0e9f05
Saad Baradan <saadbaradan@gmail.com>
2019-10-11 14:38:52 -0600

    Test that when form is submitted, preventDefault is called

--- src/components/Search/Search.test.js
+++ src/components/Search/Search.test.js
@@ -3,7 +3,7 @@ import { Search } from "./Search";
 import { shallow } from "enzyme";
 
 describe("Search", () => {
-  const e = { preventDefault: jest.fn() };
+  const e = { preventDefault: () => {} };
   let wrapper;
   const mockBikePaths = [
     {
@@ -26,18 +26,40 @@ describe("Search", () => {
   it("should match the snapshot", () => {
     expect(wrapper).toMatchSnapshot();
   });
+  it("should call preventDefault when the form is submitted", () => {
+    const mockPreventDefault = jest.fn();
+    const mockEvent = {
+      preventDefault: mockPreventDefault
+    };
+    wrapper.instance().handleSubmit(mockEvent);
+    expect(mockPreventDefault).toHaveBeenCalled();
+  });
   it("sets the state of city when user enters valid city", () => {
     expect(wrapper.state("city")).toEqual("");
     const mockEvent = { target: { value: "Pittsburgh" } };
     wrapper.instance().handleChange(mockEvent);
-    wrapper.setState({city: "Pittsburgh"})
-    expect(wrapper.state("city")).toEqual("Pittsburgh")
+    wrapper.setState({ city: "Pittsburgh" });
+    expect(wrapper.state("city")).toEqual("Pittsburgh");
   });
   it("sets the state of path when user enters valid city", () => {
     expect(wrapper.state("path")).toEqual({} || "");
     const mockEvent = { target: { value: "Pittsburgh" } };
     wrapper.instance().handleChange(mockEvent);
-    wrapper.setState({path:   {
+    wrapper.setState({
+      path: {
+        company: ["Nextbike GmbH"],
+        href: "/v2/networks/healthy-ride-pittsburgh-pittsburgh",
+        id: "healthy-ride-pittsburgh-pittsburgh",
+        location: {
+          city: "Pittsburgh",
+          country: "US",
+          latitude: 40.4459,
+          longitude: -79.9945
+        },
+        name: "Healthy Ride"
+      }
+    });
+    expect(wrapper.state("path")).toEqual({
       company: ["Nextbike GmbH"],
       href: "/v2/networks/healthy-ride-pittsburgh-pittsburgh",
       id: "healthy-ride-pittsburgh-pittsburgh",
@@ -48,25 +70,13 @@ describe("Search", () => {
         longitude: -79.9945
       },
       name: "Healthy Ride"
-    }})
-    expect(wrapper.state("path")).toEqual(  {
-      company: ["Nextbike GmbH"],
-      href: "/v2/networks/healthy-ride-pittsburgh-pittsburgh",
-      id: "healthy-ride-pittsburgh-pittsburgh",
-      location: {
-        city: "Pittsburgh",
-        country: "US",
-        latitude: 40.4459,
-        longitude: -79.9945
-      },
-      name: "Healthy Ride"
-    })
+    });
   });
   it("sets the state of path to city not found when invalid city is entered", () => {
     expect(wrapper.state("path")).toEqual({} || "");
     const mockEvent = { target: { value: "Cheyenne" } };
     wrapper.instance().handleChange(mockEvent);
-    wrapper.setState({path: "Your city was not found"})
-    expect(wrapper.state("path")).toEqual("Your city was not found")
+    wrapper.setState({ path: "Your city was not found" });
+    expect(wrapper.state("path")).toEqual("Your city was not found");
   });
 });
